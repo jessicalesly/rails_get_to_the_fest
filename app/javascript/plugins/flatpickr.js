4d5d65ce8f62f56438c0cb1c1a0cc5db17ae48d5
Christopher Cardoso <cardoso.christopher01@gmail.com>
2018-03-01 17:45:30 +0100

    final version (design only) of the search bar

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -4,8 +4,6 @@ import "flatpickr/dist/themes/confetti.css"
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 flatpickr("#range_start", {
-  dateFormat: "d-m-Y\\Z", // Displays: 2017-01-22Z
-  locale: "France",
   altInput: true,
   plugins: [new rangePlugin({ input: "#range_end"})]
 })
