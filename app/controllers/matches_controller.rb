cf624adf3063e99309ea0a1f0a19dbbda03dea43
Jessica Lesly <jk.lesly@gmail.com>
2018-03-01 11:59:58 +0100

    hash ready

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -7,7 +7,10 @@ class MatchesController < ApplicationController
     @saved_tracks_10 = spotify_user.saved_tracks(limit: 10, offset: 0)
     @saved_albums_10 = spotify_user.saved_albums(limit: 10, offset: 0)
     @top_artists = spotify_user.top_artists #=> (Artist array)
-    @top_tracks = spotify_user.top_tracks(time_range: 'short_term')
+    @top_tracks_recent = spotify_user.top_tracks(time_range: 'short_term')
+    @top_tracks_medium = spotify_user.top_tracks(time_range: 'medium_term')
+    @top_tracks_long = spotify_user.top_tracks(time_range: 'long_term')
+    @top_tracks = spotify_user.top_tracks
 
     @festival_hash = {}
 
@@ -18,7 +21,7 @@ class MatchesController < ApplicationController
     end
 
     @top_tracks_artists_array = []
-    @top_tracks.each do |top_track|
+    @top_tracks_recent.each do |top_track|
       top_track.artists.map do |artist|
         @top_tracks_artists_array << artist.name unless @top_artists_array.include?(artist.name)
       end
@@ -31,63 +34,31 @@ class MatchesController < ApplicationController
       end
     end
 
-    @top_tracks.each do |top_track|
+    @top_tracks_recent.each do |top_track|
       top_track.artists.each do |artist|
         artist.related_artists.each do |related|
           @related_artists_array << related.name unless ( @top_artists_array.include?(related.name) || @top_tracks_artists_array.include?(related.name) )
         end
       end
     end
-    #Remove duplicates de chaque liste
-    # @top_artists_array.uniq!
-    # @top_tracks_artists_array.uniq!
-    # @related_artists_array.uniq!
 
+    #Remove duplicates
+    @top_artists_array.uniq!
+    @top_tracks_artists_array.uniq!
+    @related_artists_array.uniq!
+    @all_artists = @top_artists_array + @top_tracks_artists_array + @related_artists_array
 
+    #List of festivals where there is at least one user's artist
+    @festivals = Festival.joins(line_ups: :artist).where("artists.name IN (?)", @all_artists).distinct.pluck(:name)
 
-
-
-
-    @followed_artists.each do |followed_artist|
-      artist_db = Artist.find_by(name: followed_artist.name)
-      if artist_db
-        lineups = LineUp.where(artist_id: artist_db.id)
-        lineups.each do |lineup|
-          festival_name = lineup.festival.name
-          if @festival_hash[festival_name]
-            if @festival_hash[festival_name][:followed]
-              @festival_hash[festival_name][:followed].push(artist_db.name)
-            else
-              @festival_hash[festival_name][:followed] = [artist_db.name]
-            end
-          else
-            @festival_hash[festival_name] = {followed: [artist_db.name]}
-          end
-        end
-      end
-
-      followed_artist.related_artists.each do |related_artist|
-        related_artist_db = Artist.find_by(name: related_artist.name)
-        if related_artist_db
-          lineups = LineUp.where(artist_id: related_artist_db.id)
-          lineups.each do |lineup|
-            if @festival_hash[lineup.festival.name]
-              if @festival_hash[lineup.festival.name][:related]
-                @festival_hash[lineup.festival.name][:related].push(related_artist_db.name)
-              else
-                @festival_hash[lineup.festival.name][:related] = [related_artist_db.name]
-              end
-            else
-              @festival_hash[lineup.festival.name] = {
-                related: [related_artist_db.name]
-              }
-            end
-          end
-        end
-      end
-
+    @festivals.each do |festival_name|
+      festival = Festival.where(name: festival_name).first
+      @festival_hash[festival_name] = {
+        top_artists: festival.artists.where("artists.name IN (?)", @top_artists_array).pluck(:name),
+        top_tracks_artists: festival.artists.where("artists.name IN (?)", @top_tracks_artists_array).pluck(:name),
+        related_artists: festival.artists.where("artists.name IN (?)", @related_artists_array).pluck(:name)
+      }
     end
-
 
 
   end
