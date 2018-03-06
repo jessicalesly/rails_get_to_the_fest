761aa51a2cd8afd55154d1569c7df080930cf326
Christopher Cardoso <cardoso.christopher01@gmail.com>
2018-03-06 15:11:18 +0100

    error page customize

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # get '/auth/spotify/callback', to: 'users#spotify'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
