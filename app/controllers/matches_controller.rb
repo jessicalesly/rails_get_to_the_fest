0b62f7dbd58a103194067ba42b18f54ab481a781
Jessica Lesly <jk.lesly@gmail.com>
2018-03-06 11:43:49 +0100

    algo ok

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,21 +1,19 @@
 class MatchesController < ApplicationController
-
-
   def results
-  SPOTIFY_SERVICE = SpotifyArtistsService.new(user_hash: current_user.spotify_hash)
+    rspotify = SpotifyArtistsService.new(user_hash: current_user.spotify_hash)
     @festival_array = []
     #List of festivals where there is at least one user's artist
-    @festivals = Festival.joins(line_ups: :artist).where("artists.name IN (?)", list_spotify_artists).distinct
+    @festivals = Festival.joins(line_ups: :artist).where("artists.name IN (?)", list_spotify_artists(rspotify)).distinct
     @festivals.each do |festival|
       fest_hash = {
         festival_instance: festival,
-        artists: list_artists_for_a_fest(festival),
+        artists: list_artists_for_a_fest(rspotify, festival),
       }
       fest_hash[:affinity] = 0
       fest_hash[:artists].each do |artist_hash|
-        fest_hash[:affinity] += artist_hash[:score] if artist_hash[:score]
+        fest_hash[:affinity] += artist_hash[:score]
       end
-      fest_hash[:affinity] = 100 / 25 * fest_hash[:affinity]
+      fest_hash[:affinity] = 100 / 50 * fest_hash[:affinity]
       @festival_array <<  fest_hash
     end
     @festival_array.sort_by! { |festival_hash| festival_hash[:affinity] }.reverse!
@@ -23,11 +21,11 @@ class MatchesController < ApplicationController
 
   private
 
-  def list_spotify_artists
-    SPOTIFY_SERVICE.list_artists
+  def list_spotify_artists(service)
+    service.list_artists
   end
 
-  def list_artists_for_a_fest(festival)
-    SPOTIFY_SERVICE.favorites_artists(festival)
+  def list_artists_for_a_fest(service, festival)
+    service.favorites_artists(festival)
   end
 end
--- app/services/spotify_artists_service.rb
+++ app/services/spotify_artists_service.rb
@@ -51,16 +51,24 @@ class SpotifyArtistsService
         @saved_tracks_artists_array << artist.name
       end
     end
-
     return @saved_tracks_artists_array
   end
 
   def related_artists
     @related_artists_array = []
-    top_artists_rspotify = @spotify_user.top_artists(limit: 50, offset: 0, time_range: 'short_term')
+    top_artists_rspotify = @spotify_user.top_artists(limit: 50, offset: 0, time_range: 'medium_term')
     top_artists_rspotify.each do |top_artist|
       top_artist.related_artists.each do |related_artist|
         @related_artists_array << related_artist.name unless ( @top_artists_array.include?(related_artist.name) || @top_tracks_artists_array.include?(related_artist.name) || @saved_tracks_artists_array.include?(related_artist.name) )
+      end
+    end
+    top_tracks_rspotify = @spotify_user.top_tracks(limit: 50, offset: 0, time_range: 'medium_term')
+
+    top_tracks_rspotify.each do |top_track|
+      top_track.artists.each do |artist|
+        artist.related_artists.each do |related_artist|
+          @related_artists_array << related_artist.name unless ( @top_artists_array.include?(related_artist.name) || @top_tracks_artists_array.include?(related_artist.name) || @saved_tracks_artists_array.include?(related_artist.name) )
+        end
       end
     end
     return @related_artists_array
@@ -70,35 +78,35 @@ class SpotifyArtistsService
     # @followed_artists = spotify_user.following(type: 'artist')
     # @saved_albums_10 = spotify_user.saved_albums(limit: 10, offset: 0)
     #Remove duplicates
-    sum = @top_artists_array + @top_tracks_artists_array + @saved_tracks_artists_array + @related_artists_array
-    @all_artists = sum.uniq
-    return @all_artists
+    return @all_artists ||= (@top_artists_array + @top_tracks_artists_array + @saved_tracks_artists_array + @related_artists_array).uniq
   end
 
   def favorites_artists(festival)
     festival_artists_array = []
     # raise
     festival.artists.each do |artist|
-      if @all_artists.include?(artist.name)
+      if list_artists.include?(artist.name)
         if @related_artists_array.include?(artist.name)
-          artist_hash = {name: artist.name, score: 0.5}
+          artist_hash = {name: artist.name, score: 1}
         else
           artist_hash = {
             name: artist.name,
             is_top_artist: @top_artists_array.include?(artist.name),
             nb_top_tracks: @top_tracks_artists_array.count(artist.name),
             nb_saved_tracks: @saved_tracks_artists_array.count(artist.name),
+            score: 0
           }
           artist_hash[:score] = 10 if artist_hash[:is_top_artist]
           artist_hash[:score] += 3 * artist_hash[:nb_top_tracks]
-          artist_hash[:score] += 1 * artist_hash[:nb_saved_tracks]
+          artist_hash[:score] += 2 * artist_hash[:nb_saved_tracks]
         end
       else
-        artist_hash = {name: artist.name}
+        artist_hash = {name: artist.name, score:0}
       end
       # artist_hash[:related_to].count
       festival_artists_array << artist_hash
     end
+    festival_artists_array.sort_by! { |artist_hash| artist_hash[:score] }.reverse!
     return festival_artists_array
   end
 end
