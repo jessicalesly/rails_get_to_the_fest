16e7be497f6bfa5c3a3daadfbc77a60ed1f9e81c
Christopher Cardoso <cardoso.christopher01@gmail.com>
2018-03-08 19:03:50 +0100

    adding typed.js on loader

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,18 @@
 import "gsap";
 import "../plugins/flatpickr";
 import "bootstrap";
-import {divAppear} from '../components/collapse';
+// import {divAppear} from '../components/collapse';
 
 
 import '../components/burger';
 import {flip_cards} from '../components/flip_cards';
 import { bindLoaderToSpotifyConnectButton } from '../components/loader';
 
+import { loadDynamicText } from '../components/typed-text';
+
 const spotifyConnectButton = document.getElementById('connect-spotify');
 if (spotifyConnectButton) {
+  loadDynamicText();
   bindLoaderToSpotifyConnectButton(spotifyConnectButton);
 }
 
@@ -18,4 +21,4 @@ if(cards_black) {
   flip_cards();
 }
 
-divAppear();
+// divAppear();
