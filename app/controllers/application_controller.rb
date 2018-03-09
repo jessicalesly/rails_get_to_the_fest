07671bdded78afb2b3e02a0164a59317b8ecf54d
Christopher Cardoso <cardoso.christopher01@gmail.com>
2018-03-09 10:53:27 +0100

    exercise done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-  { host: ENV["HOST"] || "localhost:3000" }
+    { host: ENV["HOST"] || "localhost:3000" }
   end
 end
